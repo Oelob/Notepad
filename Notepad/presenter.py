e83c8e84c93a3453c3b11f7c77fefd361063fd39
Администратор <administrator@MacBook-Pro-Administrator.local>
2023-04-30 23:03:34 +0500

    добавил вывод списка заметок

--- Notepad/presenter.py
+++ Notepad/presenter.py
@@ -1,26 +1,32 @@
+from operator import itemgetter
 from models import *
 
 from View import *
 import json
 
 def Menu():
-    a = int(input("Введите действие:\n 1. Добавление новой заметки \n 2. Удаление заметки \n 0. Выход\n "))
+    a = int(input("Введите действие:\
+                  \n 1. Добавление новой заметки \
+                  \n 2. Удаление заметки \
+                  \n 3. показать все заметки\
+                  \n 0. Выход\n "))
     if (a == 1):
         list = CreateNotepad.GetInfo()
         obj = Notepad(list[0],list[1],list[2])
-        print(obj)
         try: 
            with open ('data.json', 'r') as json_file:
                 data = json.load(json_file)
                 data.append(obj.GetDict())
                 with open('data.json', 'w') as f:
-                    json.dump(data, f, indent=4, sort_keys=True, ensure_ascii=False)
+                    json.dump(data, f, indent=4, ensure_ascii=False)
                     f.truncate()
                 print("Заметка добавлена")
+                Menu()
         except:
             with open('data.json', 'w') as json_file:
                 json.dump([obj.GetDict()], json_file, indent=2, ensure_ascii=False)
                 print("Заметка добавлена")
+                Menu()
 
 
     if (a == 2):
@@ -31,32 +37,34 @@ def Menu():
                 for i in data:
                     if i['id'] == num_note:
                         data.remove(i)
-                # print(data)
                 with open('data.json', 'w') as f:
-                    json.dump(data, f, indent=4, sort_keys=True, ensure_ascii=False)
+                    json.dump(data, f, indent=4, ensure_ascii=False)
                     f.truncate()
                     print("Заметка удалена")
+                    Menu()
                 
         except:
                 print('File does not exsist')
                 print('Try again')
                 Menu()
     
+    if (a == 3):
+        try: 
+            with open ('data.json', 'r') as json_file:
+                data = json.load(json_file)
+                print(type(data))
+                new_data = sorted(data, key= itemgetter("date"))
+                for i in new_data:
+                    print(i)
+                Menu()
+                
+        except:
+                print('File does not exsist')
+                print('Try again')
+                Menu()
+        
+
     if (a == 0):
         return print("The end")
 
 Menu()        
-
-# list = CreateNotepad.GetInfo()
-# obj = Notepad(list[0],list[1],list[2])
-# print(obj.GetDict())
-# print(json.dumps(obj.__dict__))
-# with open('data.json', 'w') as f:
-    
-#     json.dump([obj.__dict__], f, indent=2, ensure_ascii=False)
-   
-    
-
-# with open('data.json', 'r') as file:
-#     data = json.load(file)
-# print(data)
