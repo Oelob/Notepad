43e7777d431d2af6d5dbad2ee89f6a70ad1852fe
Администратор <administrator@MacBook-Pro-Administrator.local>
2023-05-02 00:53:00 +0500

    добавил функцию внесения изменений

--- Notepad/View.py
+++ Notepad/View.py
@@ -21,7 +21,7 @@ class DelNotepad():
         return result
 
 class ShowToUser():
-    message = ""
+    message = ''
     def __init__(self, message):
         self.message = message
 
@@ -29,4 +29,30 @@ class ShowToUser():
         print(self.message)
     
     def PrintError():
-        print('File does not exsist\n Try again')
+        message = 'File does not exsist\nTry again'
+        print(message)
+
+class GetEdit():
+    def GetId():
+        res = int(input('Введите id заметки, которую хотите изменить: '))
+        return res
+
+    def GetInfo():
+        result = []
+        # id = int(input('Введите id заметки, которую хотите изменить: '))
+        # result.append(id)
+        elemet = int(input('Выберите элемент заметки, которую хотите изменить:\
+                           \n 1. id\
+                           \n 2. title\
+                           \n 3. msg '))
+        if elemet == 1: 
+            result.append('id') 
+            result.append(int(input('Введите новое значение: ')))
+        if elemet == 2: 
+            result.append('title')
+            result.append(input('Введите новое значение: '))
+        if elemet == 3: 
+            result.append('msg')
+            result.append(input('Введите новое значение: '))
+        return result
+        
