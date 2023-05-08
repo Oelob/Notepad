1598c71fc0be62c09acd872299b95abe951669ba
Администратор <administrator@MacBook-Pro-Administrator.local>
2023-05-08 11:50:58 +0500

    поменял папку/добавил игрушку

--- game/program.java
+++ game/program.java
@@ -9,6 +9,7 @@ public class program{
         Toys toy = new Toys(1, "tiger", 5, 80, "fluffy");
         Toys toy2 = new Toys(2, "lion", 4, 60, "fluffy");
         Toys toy3 = new Toys(3, "cat", 3, 50, "plastic");
+        Toys toy4 = new Toys(4, "bycicle", 1, 20, "transport");
         
 
        
@@ -17,6 +18,7 @@ public class program{
         box.add(toy);
         box.add(toy2);
         box.add(toy3);
+        box.add(toy4);
         box.printBox(box);
 
         
